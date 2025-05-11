f283cf288fda591f275544af324c8481fe5d83ff
lindaluu <58942386+lindaluu13@users.noreply.github.com>
2025-05-12 00:33:59 +0200

    feat: choose a number of dices to roll
    The user can now choose a number between 1 and 10 of dices to roll.

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 A simple code to roll some dices !
 
 May the Odds Be Ever in Your Favor
+
 <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHl3dmIxb21oajk1ZWMwcnZ0YWUzeGFhb3k4NXR0dHg2bmNyYWRoNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3ohjUS2N88LGAjLypO/giphy.gif" width="300" height="200" />
--- rolling_dice.py
+++ rolling_dice.py
@@ -3,7 +3,19 @@ import random
 while True:
     choice = input("Do you want to roll the dice ? (y/n) : ").lower()
     if choice == 'y':
-        print("Rolling dice...\nAnd the number is... " + str(random.randint(1,6)))
+        try:
+            num_dice = int(input("How many dice do you want to roll? (between 1 to 10) : "))
+            if num_dice < 1 or num_dice > 10:
+                print("Please enter a number greater than 0 and below 10.")
+                continue
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+            continue
+
+        print("Rolling dice...")
+        results = [random.randint(1, 6) for _ in range(num_dice)]
+        print("You rolled: " + ", ".join(str(r) for r in results))
+
     elif choice == 'n':
         print("Thank you for playing! See you soon.")
         break
